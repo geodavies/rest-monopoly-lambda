676759285081ba723f2be5bde713a75ef93aca29
George Davies <george.davies@capgemini.com>
2019-09-19 20:41:27 +0100

    Implemented get player API

--- src/main/handlers/players/get.js
+++ src/main/handlers/players/get.js
@@ -2,11 +2,30 @@
 const responseGenerator = require('../../components/generator/responseGenerator');
 const common = require('../common/common');
 
-module.exports.get = (event) => {
+module.exports.players = (event) => {
   return Promise.resolve(event.pathParameters.gameId)
     .then(common.validateGameId)
     .then(common.getGameFromDatabase)
     .then((game) => game.players)
     .then(responseGenerator.generateSuccessResponse)
-    .catch(handledErrorResponse => Promise.resolve(handledErrorResponse))
+    .catch(handledErrorResponse => Promise.resolve(handledErrorResponse));
+};
+
+module.exports.player = (event) => {
+  return Promise.resolve()
+    .then(() => common.validateGameId(event.pathParameters.gameId))
+    .then(() => common.validatePlayerId(event.pathParameters.playerId))
+    .then(common.getGameFromDatabase)
+    .then((game) => getPlayerFromGame(game, event.pathParameters.playerId))
+    .then(responseGenerator.generateSuccessResponse)
+    .catch(handledErrorResponse => Promise.resolve(handledErrorResponse));
+};
+
+const getPlayerFromGame = (game, playerId) => {
+  const filteredPlayers = game.players.filter((player) => player.id === playerId);
+  if (filteredPlayers.length === 1) {
+    return filteredPlayers[0];
+  } else {
+    throw responseGenerator.generateFailureResponse(404, 'Player not found');
+  }
 };
--- src/test/handlers/players/get.test.js
+++ src/test/handlers/players/get.test.js
@@ -1,5 +1,6 @@
 'use strict';
 const chai = require('chai');
+const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const sinon = require('sinon');
@@ -9,6 +10,8 @@ const commonTests = require('../sharedHandlerTestHelpers');
 const gamesDatabaseDao = require('../../../main/components/database/gamesDatabaseDao');
 
 const get = require('../../../main/handlers/players/get');
+
+const NotFoundError = require('../../../main/components/error/NotFoundError');
 
 describe('Get Players Handler', () => {
 
@@ -22,19 +25,127 @@ describe('Get Players Handler', () => {
     const expectedResponseBody = ["test", "players"];
     const game = {players: expectedResponseBody};
 
-    commonTests.success200(get.get, sandbox, gamesDatabaseDao, game, expectedResponseBody);
+    commonTests.success200(get.players, sandbox, gamesDatabaseDao, game, expectedResponseBody);
   });
 
   it('Returns 400 if game ID is invalid', () => {
-    commonTests.invalidId400(get.get, sandbox, gamesDatabaseDao);
+    commonTests.invalidId400(get.players, sandbox, gamesDatabaseDao);
   });
 
   it('Returns 404 if no game found in database', () => {
-    commonTests.noGame404(get.get, sandbox, gamesDatabaseDao);
+    commonTests.noGame404(get.players, sandbox, gamesDatabaseDao);
   });
 
   it('Returns 502 if database call fails', () => {
-    commonTests.databaseError502(get.get, sandbox, gamesDatabaseDao);
+    commonTests.databaseError502(get.players, sandbox, gamesDatabaseDao);
+  });
+
+});
+
+describe('Get Player Handler', () => {
+
+  const sandbox = sinon.createSandbox();
+
+  const validTestId = 'ABcde12345';
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('Successfully gets a player of a game', () => {
+    const player = {id: validTestId};
+    const databaseResponse = {players: [player]};
+
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns(Promise.resolve(databaseResponse));
+
+    const result = get.player({pathParameters: {gameId: validTestId, playerId: validTestId}});
+
+    expect(databaseStub.calledOnce);
+
+    return expect(result).to.become({
+      statusCode: 200,
+      body: JSON.stringify(player)
+    });
+  });
+
+  it('Returns 400 if game ID is invalid', () => {
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns({});
+
+    const result = get.player({pathParameters: {gameId: 'INVALID UUID', playerId: validTestId}});
+
+    expect(databaseStub.notCalled);
+
+    return expect(result).to.become({
+      statusCode: 400,
+      body: JSON.stringify({
+        reason: 'Game ID is invalid'
+      })
+    });
+  });
+
+  it('Returns 400 if player ID is invalid', () => {
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns({});
+
+    const result = get.player({pathParameters: {gameId: validTestId, playerId: 'INVALID UUID'}});
+
+    expect(databaseStub.notCalled);
+
+    return expect(result).to.become({
+      statusCode: 400,
+      body: JSON.stringify({
+        reason: 'Player ID is invalid'
+      })
+    });
+  });
+
+  it('Returns 404 if no game found in database', () => {
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns(Promise.reject(new NotFoundError('TEST ERROR')));
+
+    const result = get.player({pathParameters: {gameId: validTestId, playerId: validTestId}});
+
+    expect(databaseStub.calledOnce);
+
+    return expect(result).to.become({
+      statusCode: 404
+    });
+  });
+
+  it('Returns 404 if player not found in game', () => {
+    const databaseResponse = {players: []};
+
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns(Promise.resolve(databaseResponse));
+
+    const result = get.player({pathParameters: {gameId: validTestId, playerId: validTestId}});
+
+    expect(databaseStub.calledOnce);
+
+    return expect(result).to.become({
+      statusCode: 404,
+      body: JSON.stringify({
+        reason: 'Player not found'
+      })
+    });
+  });
+
+  it('Returns 502 if database call fails', () => {
+    const databaseStub = sandbox.stub(gamesDatabaseDao, 'getById')
+      .returns(Promise.reject(new Error('TEST ERROR')));
+
+    const result = get.player({pathParameters: {gameId: validTestId, playerId: validTestId}});
+
+    expect(databaseStub.calledOnce);
+
+    return expect(result).to.become({
+      statusCode: 502,
+      body: JSON.stringify({
+        reason: 'TEST ERROR'
+      })
+    });
   });
 
 });
